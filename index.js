8c8bb87264ba0800cb24d6b1f4c4f4e224e88fc3
ALPH4 <139907753+alph4lk@users.noreply.github.com>
2024-11-21 01:24:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["👁️‍🗨️ Watching ΛLPH4 | Official", "🎧 Listening to Raganne Yakuza"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
